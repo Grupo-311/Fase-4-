82d906b97ea70a0b6710a678652534b1156693ff
Viviana Polanco <vivianalexapolanco@gmail.com>
2026-05-10 23:58:16 -0500

    Aporte de Viviana: módulos cliente, logger y main

---
+++ Estructura.py
@@ -0,0 +1,9 @@
+software_fj/
+│
+├── main.py          # Archivo principal que une todo
+├── cliente.py       # Clase Cliente (tu parte)
+├── servicio.py      # Clase abstracta Servicio + derivados
+├── reserva.py       # Clase Reserva
+├── excepciones.py   # Excepciones personalizadas
+├── logger.py        # Configuración centralizada de logs
+└── logs.txt         # Registro de errores
